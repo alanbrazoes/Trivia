2d793563e4a03df39a2d33856e12c5a65118543c
Alan Brazões <alanbrazoes@gmail.com>
2022-04-21 18:06:46 -0300

    feat: add rules page

---
+++ src/views/Rules/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Container } from './styles'
+
+const Rules: React.FC = () => {
+  return (
+    <Container>
+      <View>
+        <Text>Rules</Text>
+      </View>
+    </Container>
+  )
+}
+
+export default Rules
