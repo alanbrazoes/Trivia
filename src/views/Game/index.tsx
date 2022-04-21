a13d83487527999a9ede1111992e0741bccb18ed
Alan Brazões <alanbrazoes@gmail.com>
2022-04-21 18:12:02 -0300

    feat: add style in Gapa page

--- src/views/Game/index.tsx
+++ src/views/Game/index.tsx
@@ -1,29 +1,19 @@
 import React from 'react'
 
-import { View, SafeAreaView, StyleSheet, StatusBar } from 'react-native'
 import Questions from '../../components/Questions'
 import Score from '../../components/Score'
 import Timer from '../../components/Timer'
 import { INavigation } from '../../types'
+import { Container, ViewPage } from './style'
 
 const Game: React.FC<INavigation> = ({ navigation }) => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <View style={style.container}>
+  <Container>
+    <ViewPage>
       <Score/>
       <Questions navigation={navigation}/>
       <Timer />
-    </View>
-  </SafeAreaView>
+    </ViewPage>
+  </Container>
 )
-
-const style = StyleSheet.create({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'space-between',
-    paddingTop: StatusBar.currentHeight
-  }
-})
 
 export default Game
---
+++ src/views/Game/style.ts
@@ -0,0 +1,14 @@
+import { StatusBar } from 'react-native'
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`
+
+export const ViewPage = styled.View`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  padding: ${StatusBar.currentHeight}
+`
