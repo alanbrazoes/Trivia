94642fc160745193b5855a477c648c38831fb994
Alan Brazões <alanbrazoes@gmail.com>
2022-04-25 09:58:58 -0300

    feat: add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import StatePlayer from './context/player'
 import StateQuestion from './context/questions'
 import StateTimer from './context/timer'
 import Routes from './routes'
+import theme from './style/theme'
 
 const App = () => {
   return (
     <StatePlayer>
       <StateTimer>
         <StateQuestion>
-          <Routes />
+          <ThemeProvider theme={theme}>
+            <Routes />
+          </ThemeProvider>
         </StateQuestion>
       </StateTimer>
     </StatePlayer>
