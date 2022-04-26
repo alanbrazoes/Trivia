baccbbb5c353e33a02d9c73f2c0d1b40359e4bce
Alan Brazões <alanbrazoes@gmail.com>
2022-04-26 10:17:09 -0300

    fix: translate

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -8,9 +8,9 @@ const Result: React.FC<INavigation> = ({ navigation }) => {
   const { player: { score } } = useContext(ContextPlayer)
 
   const feedback = () => {
-    if (score >= 1800 && score < 2699) return <Text>Nice result!</Text>
-    if (score < 1800) return <Text>A little below average.</Text>
-    if (score >= 2700) return <Text>Damn, you really know!</Text>
+    if (score >= 1800 && score < 2699) return <Text>Nice result</Text>
+    if (score < 1800) return <Text>A little below average</Text>
+    if (score >= 2700) return <Text>Damn, you really know!!</Text>
   }
 
   return (
