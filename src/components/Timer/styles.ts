f2715913cd7521adfff0653ead58776bf333bf6f
Alan Brazões <alanbrazoes@gmail.com>
2022-04-27 10:17:13 -0300

    fix: unit of measurement

--- src/components/Timer/styles.ts
+++ src/components/Timer/styles.ts
@@ -4,11 +4,11 @@ export const ContainerTimer = styled.View`
     justify-content: center;
     align-items: center;
     background-color: ${({ theme }) => theme.colors.background.dark};
-    padding: 4
+    padding: 4px
 `
 
 export const Title = styled.Text`
   font-weight: bold;
-  font-size: 32;
+  font-size: 32px;
   color: ${({ theme }) => theme.colors.default.light}
 `
