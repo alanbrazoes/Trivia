a2331c8b8c65c7ca216c03b3f65b199fc357c745
Alan Brazões <alanbrazoes@gmail.com>
2022-04-30 21:35:26 -0300

    refactor: rearranging types

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { IQuestion } from '../context/types'
+import { IQuestion } from '../types/states'
 
 function useFetch (url: string) {
   const [data, setData] = useState<IQuestion[]>()
