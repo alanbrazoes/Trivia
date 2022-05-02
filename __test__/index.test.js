78eece559fc5525772275286cfb86f96c0beb6e3
Alan Brazões <alanbrazoes@gmail.com>
2022-05-02 19:47:38 -0300

    add tests setup

---
+++ __test__/index.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Home from '../src/views/Home'
+
+describe('Aleatório', () => {
+  test('1', () => {
+    renderer.create(<Home />)
+  })
+})
